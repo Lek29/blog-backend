eba359f3d0078070f9defed36814c341c5b23add
Lek29 <ligioner29@mail.ru>
2024-12-03 05:50:27 +0800

    Delete venv_3.10 directory

--- venv_3.10/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\pythonProject\pythonProject\blog-backend\venv_3.10\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
